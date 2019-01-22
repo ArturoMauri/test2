5c3d68e77d8bb3df7f4cd682f83f248f1554f468
ArturoMauri <mauri_arturo@hotmail.com>
2019-01-22 00:33:36 +0000

    Pizza

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {}
+
+config.uri = "https://azuremol.documents.azure.com:443/";
+config.primaryKey = "MMRTjSGjoyB2upCiurYDSgAsx4n1r9zsm4vUwKva0wfBC4mIpwK2z8cCwD0t88C4QkFQOgtWl749Sn0kKn7iMA==";
+config.database = { "id": "pizzadb" };
+config.collection = { "id": "pizzas" };
+
+module.exports = config;
